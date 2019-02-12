afc1de03f9a925ece8d978f90e75139c74fa986b
calledude <22471295+calledude@users.noreply.github.com>
2019-02-12 01:08:02 +0100

    Refactored SpotifyClient

--- SpotifyVolumeExtension/SpotifyClient.cs
+++ SpotifyVolumeExtension/SpotifyClient.cs
@@ -4,79 +4,35 @@ using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
 using System.Threading;
+using System.Reflection;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace SpotifyVolumeExtension
 {
-    public class SpotifyClient
+    public abstract class SpotifyClient
     {
-        private EventWaitHandle authWait = new AutoResetEvent(false);
-        private Auth auth;
-        private string _clientID = "8c35f18897a14d9c8008323a7c167c68";
-        private string _clientSecret = null; //Your Client-Secret here
+        protected readonly AutoResetEvent authWait = new AutoResetEvent(false);
+        protected readonly string _clientID = "8c35f18897a14d9c8008323a7c167c68";
+        protected readonly string _clientSecret = null; //Your Client-Secret here
 
+        public readonly SpotifyWebAPI Api;
         public event Action NoActivePlayer;
-        public SpotifyWebAPI Api { get; private set; }
 
-        public SpotifyClient(AuthType authType) //AuthType.Authorization requires your own Client-ID and Client-Secret to work.
+        protected abstract Task RefreshToken();
+        protected abstract void OnAuthResponse(object sender, Token payload);
+
+        protected SpotifyClient()
         {
-            if (authType == AuthType.Authorization && _clientSecret == null)
-                throw new InvalidOperationException("Client secret must be provided with this token-type.");
-
-            if (authType == AuthType.Implicit)
-                auth = new ImplicitGrantAuth(_clientID, "http://localhost:80", "http://localhost:80");
-            else
-                auth = new AuthorizationCodeAuth(_clientID, _clientSecret, "http://localhost:80", "http://localhost:80");
-
-            auth.Scope = Scope.UserModifyPlaybackState | Scope.UserReadPlaybackState;
-            auth.AuthReceived += OnAuthResponse;
-
             Api = new SpotifyWebAPI();
             Api.OnError += OnError;
         }
 
-        public void Start()
-        {
-            Authenticate();
-
-            Console.WriteLine("[SpotifyClient] Successfully connected.");
-        }
-
-        private void Authenticate()
-        {
-            auth.Start();
-            auth.OpenBrowser();
-
-            authWait.WaitOne(); //Wait for response
-        }
-
-        private void OnAuthResponse(object sender, Token payload)
-        {
-            Api.Token = payload;
-            auth.Stop(0);
-
-            authWait.Set();
-        }
-
-        private void RefreshToken()
-        {
-            if (auth is AuthorizationCodeAuth e)
-            {
-                Api.Token = e.RefreshToken(Api.Token.RefreshToken).Result;
-            }
-            else
-            {
-                Authenticate();
-            }
-
-            Console.WriteLine("[SpotifyClient] Refreshed token.");
-        }
-
-        private void OnError(Error error)
+        protected async void OnError(Error error)
         {
             if (Api.Token.IsExpired())
             {
-                RefreshToken();
+                await RefreshToken();
             }
             else if (error.Status == 404) // No active player
             {
@@ -84,9 +40,60 @@ namespace SpotifyVolumeExtension
             }
             else
             {
-                Console.WriteLine($"[SpotifyClient] {error.Status} {error.Message}");
+                Console.WriteLine($"[SpotifyClient] {error.Status.ToString()} {error.Message}");
             }
         }
+    }
 
+    public sealed class SpotifyClient<T> : SpotifyClient where T : Auth
+    {
+        private readonly T auth;
+
+        public SpotifyClient() //AuthorizationCodeAuth requires your own Client-ID and Client-Secret to work.
+        {
+            auth = (T)Activator.CreateInstance(typeof(T),
+                                               BindingFlags.OptionalParamBinding,
+                                               null,
+                                               new object[] { _clientID, "http://localhost:80", "http://localhost:80" },
+                                               CultureInfo.CurrentCulture);
+
+            if (auth is AuthorizationCodeAuth e)
+            {
+                e.SecretId = _clientSecret ?? throw new InvalidOperationException("Client secret must be provided with " + typeof(T));
+            }
+
+            auth.Scope = Scope.UserModifyPlaybackState | Scope.UserReadPlaybackState;
+            auth.AuthReceived += OnAuthResponse;
+        }
+
+        public void Authenticate()
+        {
+            auth.Start();
+            auth.OpenBrowser();
+
+            authWait.WaitOne(); //Wait for response
+            Console.WriteLine("[SpotifyClient] Successfully authenticated.");
+        }
+
+        protected override void OnAuthResponse(object sender, Token payload)
+        {
+            Api.Token = payload;
+            auth.Stop(0);
+
+            authWait.Set();
+        }
+
+        protected override async Task RefreshToken()
+        {
+            if (auth is AuthorizationCodeAuth e)
+            {
+                Api.Token = await e.RefreshToken(Api.Token.RefreshToken);
+            }
+            else
+            {
+                Authenticate();
+            }
+            Console.WriteLine("[SpotifyClient] Refreshed token.");
+        }
     }
 }
